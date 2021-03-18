7d27132ff2e12daa78f86e06eb5f88a3a7f08f7b
lkyero <yulikun96@163.com>
2021-03-18 14:06:20 +0800

    1

---
+++ Prototype 3/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    public Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    private float startDelay = 2.0f;
+    private float repeatRate = 2.0f;
+    private PlayerController playerController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnObstacle()
+    {
+        if (!playerController.gameOver)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+}
